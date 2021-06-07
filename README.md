7ea2f1202d845e22b0c566c5f391b4f57dbc2f7e
RyoNkmr <ryonakamuraryo@gmail.com>
2021-06-07 15:55:13 +0900

    first commit(install parcel and sample react app)

---
+++ README.md
@@ -0,0 +1,10 @@
+# The very first step of vdom
+
+1. try React app with TypeScript & parcel(v2)
+2. replace app.tsx with your vdom impl
+
+## Start
+
+```sh
+# yarn start
+```
---
+++ src/react-example.tsx
@@ -0,0 +1,28 @@
+import React, { useState, useCallback } from "react";
+import { render } from "react-dom";
+
+const BigButton: React.FC = ({ children, ...rest }) => (
+  <button style={{ fontSize: "30px" }} {...rest}>
+    {children}
+  </button>
+);
+
+const Hello: React.FC = () => {
+  const [count, setCount] = useState(0);
+  const increment = useCallback(() => {
+    setCount(count + 1);
+  }, [count, setCount]);
+  const decrement = useCallback(() => {
+    setCount(count - 1);
+  }, [count, setCount]);
+
+  return (
+    <section>
+      <h1>Hello World from {count}</h1>
+      <BigButton onClick={increment}>+</BigButton>
+      <BigButton onClick={decrement}>-</BigButton>
+    </section>
+  );
+};
+
+render(<Hello />, document.getElementById("app"));
