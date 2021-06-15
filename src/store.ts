b278b18744168867873e18d20f7061f7f3b3246b
RyoNkmr <ryonakamuraryo@gmail.com>
2021-06-15 11:53:31 +0900

    add commit and getter into Store

--- src/store.ts
+++ src/store.ts
@@ -3,22 +3,33 @@ export type MutationType<S> = {
 };
 type Mutation<M = {}> = M & { _mutationBrand: never };
 
-export type Store<S, M> = {
-  state: S;
-  mutation: M;
-};
+export class Store<S extends object, M extends MutationType<S>> {
+  private _state: S;
+  private readonly _mutation: M;
+  private _getter: S;
 
-export const createMutation = <S extends object, M extends MutationType<S>>(
-  state: S,
-  mutation: M
-): Mutation<M> => mutation as Mutation<M>;
-export const createStore = <S extends object, M extends Mutation>(
-  state: S,
-  mutation: M
-): Store<S, M> => ({
-  state,
-  mutation,
-});
+  public constructor(state: S, mutation: M) {
+    this._state = state;
+    this._mutation = mutation;
+    this._getter = new Proxy(this._state, {
+      get: (target, prop, receiver) => {
+        if (!Object.prototype.hasOwnProperty.call(this._state, prop)) {
+          console.warn(`${prop.toString()} is not a key of the state`);
+          return undefined;
+        }
+        return Reflect.get(target, prop, receiver);
+      },
+    });
+  }
+
+  public get commit(): Omit<M, "_mutationBrand"> {
+    return this._mutation as any;
+  }
+
+  public get getter(): S {
+    return this._getter;
+  }
+}
 
 export const createMutation = <S extends object, M extends MutationType<S>>(
   state: S,
@@ -28,7 +39,4 @@ export const createMutation = <S extends object, M extends MutationType<S>>(
 export const createStore = <S extends object, M extends MutationType<S>>(
   state: S,
   mutation: M
-): Store<S, M> => ({
-  state,
-  mutation,
-});
+): Store<S, M> => new Store(state, mutation);
